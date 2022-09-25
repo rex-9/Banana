2f764cb53f0bb3edfc3339ff8fe23a3d435631b6
Htet Naing <htetnaing0814@gmail.com>
2022-09-25 16:38:48 +0630

    added all data

--- src/data.js
+++ src/data.js
@@ -38,4 +38,94 @@ const movies = [{
   phrase: 'Drama: Blah Blah',
 }];
 
-export {  navItems, devices, supports, movies };
+const shopNLearn = [
+  'Store',
+  'Mac',
+  'iPad',
+  'iPhone',
+  'Watch',
+  'AirPods',
+  'TV & Home',
+  'AirTag',
+  'Accessories',
+  'Gift Cards',
+];
+
+const services = [
+  'Apple Music',
+  'Apple TV+',
+  'Apple Fitness+',
+  'Apple News+',
+  'Apple Arcade',
+  'iCloud',
+  'Apple One',
+  'Apple Card',
+  'Apple Books',
+  'Apple Podcasts',
+  'App Store',
+];
+
+const account = [
+  'Manage Your Apple ID',
+  'Apple Store Account',
+  'iCloud.com',
+];
+
+const appleStore = [
+  'Find a Store',
+  'Genius Bar',
+  'Today at Apple',
+  'Apple Camp',
+  'Apple Store App',
+  'Refurbished and Clearance',
+  'Financing',
+  'Apple Trade In',
+  'Order Status',
+  'Shopping Help',
+];
+
+const forBusiness = [
+  'Apple and Business',
+  'Shop for Business',
+];
+
+const forEducation = [
+  'Apple and Education',
+  'Shop for K-12',
+  'Shop for College',
+];
+
+const forHealthcare = [
+  'Apple in Healthcare',
+  'Health on Apple Watch',
+  'Health Records on iPhone',
+];
+
+const forGovernment = [
+  'Shop for Government',
+  'Shop for Veterans and Military',
+];
+
+const appleValues = [
+  'Accessibility',
+  'Education',
+  'Environment',
+  'Inclusion and Diversity',
+  'Privacy',
+  'Racial Equity and Justice',
+  'Supplier Responsibility',
+];
+
+const aboutApple = [
+  'Newsroom',
+  'Apple Leadership',
+  'Career Opportunities',
+  'Investors',
+  'Ethics & Compliance',
+  'Events',
+  'Contact Apple',
+];
+
+export {
+  navItems, devices, supports, movies, shopNLearn, services, account, appleStore, forBusiness, forEducation, forHealthcare, forGovernment, appleValues, aboutApple,
+};
