0000ff740e12fd8e8a429e7ba2f88fde3581fca8
Htet Naing <htetnaing0814@gmail.com>
2022-09-25 15:47:05 +0630

    desktop nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,25 @@
+import apple from '../assets/apple.svg';
+import search from '../assets/search.svg';
+import bag from '../assets/bag.png';
+import './Nav.scss';
+
 const Nav = () => (
   <>
-    <div>Nav</div>
+    <div className="nav">
+      <img height={20} src={apple} alt="apple" />
+      <span>Store</span>
+      <span>Mac</span>
+      <span>iPad</span>
+      <span>iPhone</span>
+      <span>Watch</span>
+      <span>AirPods</span>
+      <span>TV & Home</span>
+      <span>Only on Apple</span>
+      <span>Accessories</span>
+      <span>Support</span>
+      <img height={20} src={search} alt="search" />
+      <img height={20} src={bag} alt="bag" />
+    </div>
   </>
 );
 
