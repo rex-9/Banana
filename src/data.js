7ff0c8309202945822661f1b89991b31002c7a95
Htet Naing <htetnaing0814@gmail.com>
2022-09-25 14:21:25 +0630

    set up

---
+++ src/data.js
@@ -0,0 +1,7 @@
+const movies = [{
+  status: 'New Released',
+  image: 'https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg',
+  phrase: 'Drama: Blah Blah',
+}];
+
+export default movies;
